3c2a2b07198aefc28937aa433b240e2fa02df114
zs <zs@163.com>
2016-08-29 15:19:14 +0800

    第一步

---
+++ LimitFree/LimitFree/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LimitFree
+//
+//  Created by qianfeng on 16/8/29.
+//  Copyright © 2016年 qianfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LimitFree/LimitFree/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LimitFree
+//
+//  Created by qianfeng on 16/8/29.
+//  Copyright © 2016年 qianfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
